e526142e04e61e2059068c476d6ad81917fc8816
Eric Allam <eallam@icloud.com>
2024-04-04 14:47:15 +0100

    Adding a separate backend package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.ignoreWarnings = [{ module: /opentelemetry/ }];
+    }
+    return config;
+  },
 };
--- packages/trigger/src/example.ts
+++ packages/trigger/src/example.ts
@@ -1,5 +1,5 @@
 import { logger, task, wait } from "@trigger.dev/sdk/v3";
-import { createGraph } from "@repo/dsl";
+import { createGraph, callAssertNever } from "@repo/dsl";
 
 export const helloWorldTask = task({
   id: "hello-world",
@@ -10,5 +10,7 @@ export const helloWorldTask = task({
 
     // Create a new directed graph
     var g = createGraph();
+
+    callAssertNever();
   },
 });
